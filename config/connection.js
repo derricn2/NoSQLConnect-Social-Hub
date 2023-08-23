d1f9b54225b171cb6c2c32f5e2add3beea5fef70
derricn2 <derricn2@gmail.com>
2023-08-22 20:35:47 -0700

    updated connection to MongoDB database

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/social_network_db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 })
 
 module.exports = mongoose.connection;
